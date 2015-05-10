60f7f9be2571214f4fd6053c14d098dc9e9a3601
Andrew Lobanov <spline@rooker.ru>
2015-05-10 10:35:19 +0500

    Fix mail_add()

--- py/fetch.py
+++ py/fetch.py
@@ -67,18 +67,18 @@ def mail_rebuild():
 def mail_add():
     if os.path.exists("../.newmsg"):
         msgs = open("../.newmsg", "r").read().split("\n")
-        f = open("../mail/" + echo + "/0000.txt", "a")
         for msgn in msgs:
             if msgn:
                 msg = open("../base/msg/%s" % msgn, "r").read().split("\n")
                 echo = msg[1]
+                f = open("../mail/" + echo + "/0000.txt", "a")
                 n = os.listdir("../mail/%s" % echo)
                 n.sort()
                 n = str(int(n.pop().replace(".txt", "")) + 1).zfill(4) + ".txt"
                 buf = msgn + "\nОт:   " + msg[3] + " [" + msg[4] + "] " + time.strftime("%Y.%m.%d %H:%M", time.gmtime(int(msg[2]))) + " GMT\nКому: " + msg[5] + "\nТема: " + msg[6] + "\n\n" + "\n".join(msg[8:])
                 open("../mail/%s/%s" % (echo, n), "w").write(buf)
                 f.write("== " + n + " ==================== " + buf + "\n\n\n")
-        f.close()
+                f.close()
 
 def newmsg():
     if os.path.exists("../newmsg.txt"):
